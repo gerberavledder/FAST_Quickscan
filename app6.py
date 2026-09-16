8fc9ef66128128cd969da91cb026036048144b0a
GerberaVledder <115543026+gerberavledder@users.noreply.github.com>
2026-09-16 15:46:24 +0200

    Update app6.py

--- app6.py
+++ app6.py
@@ -487,7 +487,7 @@ with right:
                     tickmode="array",
                     tickvals=categories,
                     ticktext=colored_ticktext,
-                    tickfont=dict(size=20),
+                    tickfont=dict(size=11),
                 ),
             ),
             showlegend=True,
